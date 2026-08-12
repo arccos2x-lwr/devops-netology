4b86b454eff8f442e9127908e1968475c92663d8
LWR <arccos2x@gmail.com>
2026-08-12 22:51:09 +0300

    git-rebase 1

--- branching/rebase.sh
+++ branching/rebase.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 # display command line options
-
 count=1
 for param in "$@"; do
-    echo "\$@ Parameter #$count = $param"
+    echo "Next parameter: $param"
     count=$(( $count + 1 ))
 done
 
